532ce3f3a31de8f31b643eb20d3f326c9c59b03d
OfficialEmmanuelSamuel <official.emmanuel.samuel@gmail.com>
2026-02-05 00:43:46 +0100

    Secure Firebase API Keys with env Variables In the recent commit, we have updated the firebase.js file to use environment variables for storing sensitive

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUabr1DG1zILv2TnG7hrFkqhkCPT0c60g",
-  authDomain: "christianah-client-review.firebaseapp.com",
-  projectId: "christianah-client-review",
-  storageBucket: "christianah-client-review.firebasestorage.app",
-  messagingSenderId: "991283370275",
-  appId: "1:991283370275:web:980644274ec7a63b611496",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN, 
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID , 
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID ,
   measurementId: "G-L8M3YM1L5L",
 };
 
